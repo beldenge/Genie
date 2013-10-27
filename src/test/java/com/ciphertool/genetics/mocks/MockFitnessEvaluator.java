4c4b52ca327de170278d5327d13239d4e88436b6
george <george@10.0.0.7>
2013-10-27 19:16:08 -0400

    Added missing license information

--- src/test/java/com/ciphertool/genetics/mocks/MockFitnessEvaluator.java
+++ src/test/java/com/ciphertool/genetics/mocks/MockFitnessEvaluator.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2013 George Belden
+ * 
+ * This file is part of ZodiacGenetics.
+ * 
+ * ZodiacGenetics is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * ZodiacGenetics is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * ZodiacGenetics. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.ciphertool.genetics.mocks;
 
 import com.ciphertool.genetics.entities.Chromosome;
@@ -15,5 +34,4 @@ public class MockFitnessEvaluator implements FitnessEvaluator {
 	public void setGeneticStructure(Object geneticStructure) {
 		this.geneticStructure = geneticStructure;
 	}
-
 }
